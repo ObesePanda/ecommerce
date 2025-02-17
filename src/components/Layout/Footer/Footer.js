30d00dded5c0414b96cf16c0b2b6f157638103d0
ObesePanda <nava.saidalfredo@gmail.com>
2025-02-17 15:51:55 -0600

    Footer Update

--- src/components/Layout/Footer/Footer.js
+++ src/components/Layout/Footer/Footer.js
@@ -7,11 +7,44 @@ export function Footer() {
     <div className={styles.footer}>
       <Container>
         <div className={styles.columns}>
+          <div className={styles.social}>
+            <Button
+              as=""
+              href="#"
+              rectangular
+              color="facebook"
+              icon="facebook"
+            />
+            <Button
+              as=""
+              href="#"
+              rectangular
+              color="instagram"
+              icon="instagram"
+            />
+            <Button as="" href="#" rectangular color="youtube" icon="youtube" />
+          </div>
+
           <div>
+            <ul className={styles.footer_menu}>
+              <Link href="#">Contacto</Link>
+              <Link href="#">Términos y condiciones</Link>
+              <Link href="#">Devoluciones</Link>
+            </ul>
+          </div>
+
+          <div className={styles.footer_logos}>
+            <Link href="/">
+              <Image src="images/logo.png" alt="Logo" />
+            </Link>
             <Link href="/">
               <Image src="images/logo.png" alt="Logo" />
             </Link>
           </div>
+        </div>
+
+        <div className={styles.copyright}>
+          <span>Copyright © 2025 - Todos los derechos reservados</span>
         </div>
       </Container>
     </div>
--- src/components/Layout/Menu/Menu.js
+++ src/components/Layout/Menu/Menu.js
@@ -3,17 +3,20 @@ import { useState, useEffect } from "react";
 import Link from "next/link";
 import { Platform } from "@/api";
 import { map } from "lodash";
-import { Image, Icon, Input, Button } from "semantic-ui-react";
+import { Image, Icon, Input } from "semantic-ui-react";
 import { ENV } from "@/utils";
 import classNames from "classnames";
 
 const platformCrl = new Platform();
+
 export function Menu(props) {
   const { isOpenSearch } = props;
   const [platforms, setPlatforms] = useState(null);
   const [showSearch, setShowSearch] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false); // Estado para el menú móvil
 
   const openCloseSearch = () => setShowSearch((prevState) => !prevState);
+  const toggleMenu = () => setIsMenuOpen((prev) => !prev); // Alternar menú móvil
 
   useEffect(() => {
     (async () => {
@@ -27,39 +30,51 @@ export function Menu(props) {
   }, []);
 
   return (
-    <div className={styles.platforms}>
-      {map(platforms, (platform) => (
-        <Link key={platform.id} href={`/games/${platform.slug}`}>
-          <Image
-            src={`${ENV.SERVER_HOST}${platform.icon.url}`}
-            alt={platform.icon.alternativeText || platform.title}
-            width={platform.icon.width}
-            height={platform.icon.height}
-          />
-          {platform.title}
-        </Link>
-      ))}
-
-      <button className={styles.search} onClick={openCloseSearch}>
-        <Icon name="search" />
+    <div className={styles.menuContainer}>
+      {/* Botón Hamburguesa (solo en móviles) */}
+      <button className={styles.hamburger} onClick={toggleMenu}>
+        <Icon name={isMenuOpen ? "close" : "bars"} />
       </button>
 
+      {/* Menú principal */}
       <div
-        className={classNames(styles.inputContainer, {
-          [styles.active]: showSearch,
-        })}
+        className={classNames(styles.platforms, { [styles.open]: isMenuOpen })}
       >
-        <Input
-          id="search-games"
-          placeholder="Buscar"
-          className={styles.input}
-          focus={true}
-        />
-        <Icon
-          name="close"
-          className={styles.closeInput}
-          onClick={openCloseSearch}
-        />
+        {map(platforms, (platform) => (
+          <Link key={platform.id} href={`/games/${platform.slug}`}>
+            <Image
+              src={`${ENV.SERVER_HOST}${platform.icon.url}`}
+              alt={platform.icon.alternativeText || platform.title}
+              width={platform.icon.width}
+              height={platform.icon.height}
+            />
+            {platform.title}
+          </Link>
+        ))}
+
+        {/* Botón de búsqueda */}
+        <button className={styles.search} onClick={openCloseSearch}>
+          <Icon name="search" />
+        </button>
+
+        {/* Input de búsqueda */}
+        <div
+          className={classNames(styles.inputContainer, {
+            [styles.active]: showSearch,
+          })}
+        >
+          <Input
+            id="search-games"
+            placeholder="Buscar"
+            className={styles.input}
+            focus={true}
+          />
+          <Icon
+            name="close"
+            className={styles.closeInput}
+            onClick={openCloseSearch}
+          />
+        </div>
       </div>
     </div>
   );
